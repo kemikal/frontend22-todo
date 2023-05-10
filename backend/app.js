a8dab155f3f20c32d97884a4051bf225d4f787c5
kemikal <janne@kemikal.org>
2023-05-10 10:09:40 +0200

    data in json files

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require("cors");
+const fs = require("fs");
 
 const userRoute = require("./routes/users");
+const { fstat } = require('fs');
 
 var app = express();
 
@@ -17,12 +19,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/user", userRoute);
 
-let todos = [
-    {"id": 1,
-"todo": "Användarhantering",
-"done": false,
-"user": "68f7216c-b9ca-496d-9850-c746459c8812"}
-];
+// let todos = [
+//     {"id": 1,
+// "todo": "Användarhantering",
+// "done": false,
+// "user": "68f7216c-b9ca-496d-9850-c746459c8812"}
+// ];
 
 //
 // TODOS
@@ -43,43 +45,75 @@ app.get("/todo", (req, res) => {
 
 app.get("/todo/:user", (req, res) => {
 
-    let user = req.params.user;
-    let leftTodo = todos.filter(todo => todo.done == false && todo.user == user);
+    fs.readFile("./todo.json", (err, data) => {
+        if (err) console.log(("err", err));
 
-    res.json(leftTodo)
+        let todos = JSON.parse(data);
+
+        let user = req.params.user;
+        let leftTodo = todos.filter(todo => todo.done == false && todo.user == user);
+    
+        res.json(leftTodo)
+
+    })
+
 })
 
 app.post("/todo", (req, res) => {
     // TA EMOT BODY, SPARA NY TODO
 
-    let addTodo = req.body;
-    console.log("body", addTodo);
+    fs.readFile("./todo.json", (err, data) => {
 
-    let newTodo = {
-        id: todos.length + 1,
-        todo: addTodo.todo,
-        done: false,
-        user: addTodo.user
-    }
+        if (err) console.log("err", err);
 
-    todos.push(newTodo)
+        let todos = JSON.parse(data);
+        
+        let addTodo = req.body;
+        console.log("body", addTodo);
+    
+        let newTodo = {
+            id: todos.length + 1,
+            todo: addTodo.todo,
+            done: false,
+            user: addTodo.user
+        }
+    
+        todos.push(newTodo)
 
-res.json(todos)   
+        fs.writeFile("./todo.json", JSON.stringify(todos, null, 4), (err) => {
+            if (err) console.log("err", err);
+        })
+    
+        res.json(todos)   
+    })
+
 })
 
 app.post("/todo/done", (req, res) => {
 
-    // FÅNGA VILKEN TODO SOM ÄR KLAR
-    let doneTodo = req.body.id;
-    console.log("done ID", doneTodo);
-    // LETA UPP I DATAT
-    let findTodo = todos.find(todo => todo.id == doneTodo);
-    console.log("findTodo", findTodo);
-    // ÄNDRA
-    findTodo.done = true;
-    // SPARA
+    fs.readFile("./todo.json", (err, data) => {
+        if (err) console.log("err", err);
 
-    res.json(todos)
+        let todos = JSON.parse(data);
+
+        // FÅNGA VILKEN TODO SOM ÄR KLAR
+        let doneTodo = req.body.id;
+        console.log("done ID", doneTodo);
+        // LETA UPP I DATAT
+        let findTodo = todos.find(todo => todo.id == doneTodo);
+        console.log("findTodo", findTodo);
+        // ÄNDRA
+        findTodo.done = true;
+        // SPARA
+    
+        fs.writeFile("./todo.json", JSON.stringify(todos, null, 4), (err) => {
+            if (err) console.log("err", err);
+        })
+
+        res.json(todos)
+
+    })
+
 })
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
 
-let users = [];
+// let users = [];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,26 +14,51 @@ router.post("/", (req, res) => {
 
   // KOLLA FÖRST OM EN USER EXISTERAR MED DENNA EPOST
 
-  let newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    id: uuidv4()
-  }
+  // HÄMTA
 
-  users.push(newUser);
+  fs.readFile("./users.json", (err, data) => {
+    if (err) console.log("err save user", err);
+
+    let users = JSON.parse(data);
+    console.log("readfile", users);
+
+    // ÄNDRA
+    let newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      id: uuidv4()
+    }
+
+    users.push(newUser);
+
+    // SPARA
+    fs.writeFile("./users.json", JSON.stringify(users, null, 4), (err) => {
+      if (err) console.log("err", err);
+    })
 
   res.json(newUser.id);
+  })
+
+   
 })
 
 router.post("/login", (req, res) => {
 
-  let foundUser = users.find(user => user.email == req.body.email && user.password == req.body.password);
+  fs.readFile("./users.json", (err, data) => {
+    if (err) console.log("err", err);
 
-  if (foundUser) {
-    res.json({id: foundUser.id});
-  } else {
-    res.status(401).json({message: "fel inlogg"})
-  }
+    let users = JSON.parse(data);
+    
+    let foundUser = users.find(user => user.email == req.body.email && user.password == req.body.password);
+  
+    if (foundUser) {
+      res.json({id: foundUser.id});
+    } else {
+      res.status(401).json({message: "fel inlogg"})
+    }
+
+  })
+
 
 })
 
